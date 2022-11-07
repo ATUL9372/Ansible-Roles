5580be9f44cd4582c0e299fa9648acb1e7f78aea
ATUL9372 <atulyadav9372@gmail.com>
2022-11-07 16:47:11 +0530

    Ansible playbook run script on remote server send output in host machine

---
+++ server-script.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+sudo apt install curl
+echo "curl installed"
+echo "=========================================================================="
+echo "curl ifconfig"
+sudo curl ifconfig.me
+echo "=========================================================================="
